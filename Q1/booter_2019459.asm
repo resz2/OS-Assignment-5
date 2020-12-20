0329544951ac59c812133ee3674fe4ddbe640c74
resz <aryan19459@iiitd.ac.in>
2020-12-20 23:44:09 +0530

    part 1 complete

--- Q1/booter_2019459.asm
+++ Q1/booter_2019459.asm
@@ -6,15 +6,15 @@ org 0x7c00
 
 boot:
 	mov ax, 0x2401
-	int 0x15
+	int 0x15 ; enabling A20 bit
 	mov ax, 0x3
-	int 0x10
+	int 0x10 ; set vga text mode 3
 	cli
-	lgdt [gdt_pointer]
+	lgdt [gdt_pointer] ; loading gdt
 	mov eax, cr0
-	or eax,0x1
+	or eax, 0x1 ; setting 1st bit of cr0
 	mov cr0, eax
-	jmp CODE_SEG:booter
+	jmp CODE_SEG: booter
 gdt_start:
 	dq 0x0
 gdt_code:
@@ -33,8 +33,8 @@ gdt_data:
 	db 0x0
 gdt_end:
 gdt_pointer:
-	dw gdt_end - gdt_start
-	dd gdt_start
+	dw gdt_end - gdt_start ; size of gdt
+	dd gdt_start ; pointer to structure
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
@@ -42,26 +42,21 @@ DATA_SEG equ gdt_data - gdt_start
 bits 32
 booter:
 	mov ax, DATA_SEG
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	mov esi,hello
-	mov ebx,0xb8000
+	mov esi, hello
+	mov ebx, 0xb8000
 .loop1:
 	lodsb
-	or al,al
+	or al, al
 	jz .setup
-	or eax,0x0100
+	or eax, 0x0f00
 	mov word [ebx], ax
-	add ebx,2
+	add ebx, 2
 	jmp .loop1
 .setup:
 	mov edx, cr0
 	mov ecx, 32
-.loop2
-	mov eax, 0x130
+.loop2:
+	mov eax, 0xf30
 	shl edx, 1
 	adc eax, 0
 	mov [ebx], ax
